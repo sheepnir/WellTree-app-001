236901acae8bd5b50627314824893865626d3f23
Nir Sheep <sheep.nir@gmail.com>
2023-05-29 07:52:50 -0700

    Initial Commit

---
+++ WellTree01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WellTree01
+//
+//  Created by Nir Sheep on 5/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
